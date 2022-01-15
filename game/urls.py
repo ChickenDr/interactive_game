b1758ad94edc32db6779a132bc5ca86758b554ea
Dong_su <fkdlrj1230@gmail.com>
2022-01-16 02:10:35 +0900

    [#3] 캠 화면 url 연결

---
+++ game/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index_page, name = 'index_page'),
+    path("web_cam/", views.get_web_cam, name = "web_cam_page")
+]
