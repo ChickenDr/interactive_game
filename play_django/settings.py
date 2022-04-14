c35709aa6a61c098edc9b923ca44b52273a30a73
Dong_su <fkdlrj1230@gmail.com>
2022-04-14 12:53:42 +0900

    end

--- play_django/settings.py
+++ play_django/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
